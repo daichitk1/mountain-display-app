43eccceea50cf5dba8a95deb15af4676a0e2cf3c
daichi takei <dadewujing0@gmail.com>
2025-03-16 12:26:50 +0900

    update: classをclassNameに

--- src/MountainCard.jsx
+++ src/MountainCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const MountainCard = (props) => (
-  <div class="m-3 grid grid-cols-1 flex items-center justify-center">
-    <div class="mt-5 text-2xl font-semibold">{props.name}</div>
+  <div className="m-3 grid grid-cols-1 flex items-center justify-center">
+    <div className="mt-5 text-2xl font-semibold">{props.name}</div>
     <div>({props.nameKana})</div>
-    <div class="m-3 text-1xl">({props.prefectures})</div>
-    <div class="m-1 text-1xl">地域: {props.area}</div>
-    <div class="m-1 text-1xl">標高: {props.elevation}m</div>
+    <div className="m-3 text-1xl">({props.prefectures})</div>
+    <div className="m-1 text-1xl">地域: {props.area}</div>
+    <div className="m-1 text-1xl">標高: {props.elevation}m</div>
   </div>
 );
 export default MountainCard;
